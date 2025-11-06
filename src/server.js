96978b09481aae1d0fef68f1af733c52331c228c
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-11-06 08:14:22 +0100

    Remove server binding to localhost

--- src/server.js
+++ src/server.js
@@ -40,8 +40,7 @@ const swaggerOptions = {
 
 async function init() {
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
+    port: process.env.PORT || 3000,
   });
 
   const result = dotenv.config();
